161319decf250659c6d4429b0ef82c58e1b0d968
Allan Zhou <allanlinzhou@gmail.com>
2021-10-04 17:06:59 -0400

    Updated Times Table

--- Zhou_Allan_Times_Tables.py
+++ Zhou_Allan_Times_Tables.py
@@ -7,7 +7,7 @@ print("TIMES TABLE\n")
 
 MAX_ROWS = 20 
 MAX_COLUMNS = 15
-PADDING = 5
+PADDING = 5  # Spacing around each number of the times table.
 
 # Ensure that the user enters an integer. Keep asking for input until 
 # user enters integer. 
@@ -16,17 +16,17 @@ while True:
         rows = int(input("Enter the number of rows (maximum is 20): "))
         columns = int(input("Enter the number of columns (maximum is 15): "))
 
-        # Check if numbers are within the dimension boundaries (maximum
-        # 20 rows, 15 columns)
+        # Check if numbers are within dimension boundaries (maximum 20 rows, 
+        # 15 columns). If within boundaries, the while loop will be exited.
         if rows > MAX_ROWS and columns > MAX_COLUMNS: 
             print("Please lower the number of rows and columns.\n") 
-            continue 
+
         elif rows > MAX_ROWS:
             print("Please lower the number of rows.\n") 
-            continue 
+
         elif columns > MAX_COLUMNS:
             print("Please lower the number of columns.\n")
-            continue
+
         else: 
             break 
 
@@ -45,16 +45,18 @@ for i in range(1, columns + 1):
 
 # Print 5 dashes per number on the line below, since each column 
 # has a width of 5. 
-print("\n    ", "-" * PADDING * columns) 
+print("\n" + " " * PADDING + "-" * PADDING * columns) 
 
-# Print the row number and all the multiples of it up to the nth 
-# multiple, where n is columns, all with a width of 5. 
+# Print the row number and all the multiples up to the nth multiple,
+# where n is the column number, all with a spacing of 5. "i" represents
+# the row number and "j" represents the column number.
 for i in range(1, rows + 1): 
     # Use width = PADDING - 2, since there is a " |" after the row number. 
     print("{:{width}} |".format(i, width = PADDING - 2), end = '')
 
+    # The number being printed will be row number x column number. 
     for j in range(1, columns + 1):
-        print("{:{width}}".format(j * i, width = PADDING), end = '')
+        print("{:{width}}".format(i * j, width = PADDING), end = '')
 
     print() # Move to next row of numbers.
 
