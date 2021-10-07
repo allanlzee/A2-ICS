7ae907026b55228ee2a8a78eaa13a7fce3880352
Allan Z <74578068+allanlzee@users.noreply.github.com>
2021-10-07 10:48:56 -0400

    Update Zhou_Allan_Times_Tables.py

--- Zhou_Allan_Times_Tables.py
+++ Zhou_Allan_Times_Tables.py
@@ -12,67 +12,65 @@ MAX_COLUMNS = 15
 # Spacing around each number of the times table.
 PADDING = 5
 
-# Ensure that the user enters an integer. Keep asking for input until 
-# user enters integer values for rows and columns. 
+# Ensure that the user enters valid input (positive integer).
 while True: 
     try: 
-        rows = int(input("Enter the number of rows (maximum is 20): "))
-
-        # Check if rows is within the maximum boundary of 20 rows
+        rows = int(input("Enter the number of rows (1 to 20): "))
+        
+        # Check if rows is within the maximum boundary of 20 rows.
         if rows > MAX_ROWS:
             print("Please lower the number of rows.\n") 
             continue
 
-        # Check if rows is positive
+        # Rows cannot be negative integers.
         elif rows <= 0: 
             print("Please input a positive integer for rows.\n")
             continue
 
-        columns = int(input("Enter the number of columns (maximum is 15): "))
+        columns = int(input("Enter the number of columns (0, 15): "))
 
-        # Check if columns is within the maximum boundary of 15 columns
+        # Check if columns is within the maximum boundary of 15 columns.
         if columns > MAX_COLUMNS:
             print("Please lower the number of columns.\n")
             continue
         
-        # Check if columns is positive
+        # Columns cannot be negative integers. 
         elif columns <= 0: 
             print("Please input a positive integer for columns.\n")
             continue
         
-        # While Loop ends when valid input is entered.
         break
 
+    # Catch invalid inputs to keep the program from crashing. 
     except ValueError:  
         print("Not an integer! Try again.\n")
 
 print("\nMultiplication Table of {} by {}.\n".format(rows, columns)) 
 
-# Each column has a width of 5. These spaces align the row of dashes
-# with the columns of numbers, since the column is reserved for row numbers.
+# Each column has a width of 'PADDING'. The first column has will be 
+# spaces only for alignment. 
 print(" "*PADDING, end='') 
 
 # Print the numbers 1 to columns, including columns itself. 
 for i in range(1, columns + 1): 
     print("{:{width}}".format(i, width=PADDING), end='')
 
-# Print 5 dashes per column on the line below, since each column 
-# has a width of 5. The first column has no dashes, so print spaces. 
+# Print dashes for each column (of width 'PADDING') on the line below. 
+# The first column has no dashes, so print spaces. 
 print("\n" + " "*PADDING + "-"*PADDING*columns) 
  
-# Print the row number and all the multiples up to the nth multiple,
-# where n is the column number, all with spacing. "i" represents
-# the row number and "j" represents the column number.
+# "i" represents the row number and "j" represents the column number.
 for i in range(1, rows + 1): 
 
     # Use width = PADDING - 2, since there is " |" (2 characters) 
     # after the row number. 
     print("{:{width}} |".format(i, width=PADDING-2), end='')
 
+    # Print the multiples up to the nth multiple, where n is the
+    # column number, all with spacing of 'PADDING'.
     for j in range(1, columns + 1):
         print("{:{width}}".format(i * j, width=PADDING), end='')
 
-    # Move to next row of numbers.
     print() 
 
 print()
